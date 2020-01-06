e634e445f7e1f0391fc84760cfd097664ff4669e
maruthi22 <59257547+maruthi22@users.noreply.github.com>
2020-01-06 19:14:11 +0530

    Add files via upload

---
+++ Deals.java
@@ -0,0 +1,14 @@
+package com.example.codingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deals);
+    }
+}
