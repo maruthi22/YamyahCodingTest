8c1bc5820ca8658c0bee8b5574be8388954b5370
maruthi22 <jettymaruthi22@gmail.com>
2020-02-09 14:38:58 +0530

    Changed Icon

--- Project/app/src/main/java/com/example/codingtest/homeActivity.java
+++ Project/app/src/main/java/com/example/codingtest/homeActivity.java
@@ -128,20 +128,13 @@ public class homeActivity extends AppCompatActivity implements View.OnClickListe
                             }
 
                             new AlertDialog.Builder(homeActivity.this)
-                                    .setTitle("Data")
+                                    .setTitle("Data from Firebase Firestore")
                                     .setMessage(myB.toString())
 
-                                    // Specifying a listener allows you to take an action before dismissing the dialog.
-                                    // The dialog is automatically dismissed when a dialog button is clicked.
-                                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            // Continue with delete operation
-                                        }
-                                    })
 
                                     // A null listener allows the button to dismiss the dialog and take no further action.
                                     .setNegativeButton(android.R.string.no, null)
-                                    .setIcon(android.R.drawable.ic_dialog_alert)
+                                    .setIcon(android.R.drawable.stat_sys_upload)
                                     .show();
 
                         } else {
